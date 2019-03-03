8bec6b6d51666a19973e8129cbd4678d3d7b2370
hongmo <tesung30@naver.com>
2019-03-03 20:59:19 +0900

    Change Profile HTML

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,7 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out" });
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detailt" });
+export const editDetail = (req, res) => res.render("editDetail", { pageTitle: "Edit Detailt" });
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
