d63a9d45f1d66ac44410ff60d68d91d0f007190e
hongmo <tesung30@naver.com>
2019-03-03 21:22:27 +0900

    Join Controller

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,4 +1,7 @@
-export const home = (req, res) => res.render("home", {pageTitle: "home"});
+import {videos} from "../db"
+export const home = (req, res) => {
+    res.render("home", {pageTitle: "home", videos});
+}
 export const search = (req, res) => {
     const {
         query: { term: searchingBy }
@@ -6,7 +9,7 @@ export const search = (req, res) => {
     console.log(req.query.term);
     res.render("search", {pageTitle: "Search", searchingBy: searchingBy});
 }
-export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
+// export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
 export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
---
+++ db.js
@@ -0,0 +1,74 @@
+export const videos = [
+    {
+        id:3332,
+        title: "Video Aswome",
+        description: "Thie is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: "tesung30",
+            name: "hongmo",
+            email: "tesung30@naver.com"
+        }
+    },
+    {
+        id:3332,
+        title: "Video crazy",
+        description: "Thie is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: "tesung30",
+            name: "hongmo",
+            email: "tesung30@naver.com"
+        }
+    },
+    {
+        id:3332,
+        title: "Video nice",
+        description: "Thie is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: "tesung30",
+            name: "hongmo",
+            email: "tesung30@naver.com"
+        }
+    },
+    {
+        id:3332,
+        title: "Video perfect",
+        description: "Thie is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: "tesung30",
+            name: "hongmo",
+            email: "tesung30@naver.com"
+        }
+    },
+    {
+        id:3332,
+        title: "Video super",
+        description: "Thie is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: "tesung30",
+            name: "hongmo",
+            email: "tesung30@naver.com"
+        }
+    },
+    {
+        id:3332,
+        title: "Video yeah",
+        description: "Thie is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: "tesung30",
+            name: "hongmo",
+            email: "tesung30@naver.com"
+        }
+    }
+]
