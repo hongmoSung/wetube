1666dbd397b57abb4537d3fd4aabad1cfae23e55
hongmo <tesung30@naver.com>
2019-03-09 17:56:55 +0900

    Connecting MongoDB

--- db.js
+++ db.js
@@ -1,74 +1,17 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect(
+    "mongodb://localhost:27017/we-tube",
     {
-        id:3332,
-        title: "Video Aswome",
-        description: "Thie is something I love",
-        views: 24,
-        videoFile: "https://archieve.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: "tesung30",
-            name: "hongmo",
-            email: "tesung30@naver.com"
-        }
-    },
-    {
-        id:3332,
-        title: "Video crazy",
-        description: "Thie is something I love",
-        views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: "tesung30",
-            name: "hongmo",
-            email: "tesung30@naver.com"
-        }
-    },
-    {
-        id:3332,
-        title: "Video nice",
-        description: "Thie is something I love",
-        views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: "tesung30",
-            name: "hongmo",
-            email: "tesung30@naver.com"
-        }
-    },
-    {
-        id:3332,
-        title: "Video perfect",
-        description: "Thie is something I love",
-        views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: "tesung30",
-            name: "hongmo",
-            email: "tesung30@naver.com"
-        }
-    },
-    {
-        id:3332,
-        title: "Video super",
-        description: "Thie is something I love",
-        views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: "tesung30",
-            name: "hongmo",
-            email: "tesung30@naver.com"
-        }
-    },
-    {
-        id:3332,
-        title: "Video yeah",
-        description: "Thie is something I love",
-        views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: "tesung30",
-            name: "hongmo",
-            email: "tesung30@naver.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-]
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅     Connected to DB");
+const handleError = error => console.log(`❌     Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
