7564f62e072cc37cb6910c3888981fa555ede8b2
hongmo <tesung30@naver.com>
2019-03-24 17:24:47 +0900

    Github Log In Part three

--- passport.js
+++ passport.js
@@ -1,15 +1,20 @@
 import passport from "passport";
 import GithubStrategy from "passport-github";
 import User from "./models/User";
+import { githubLoginCallback } from "./controller/userController";
+import routes from "./routes";
 
 passport.use(User.createStrategy());
 
 passport.use(
-  new GithubStrategy({
-    clientID: process.env.GH_ID,
-    clientSecret: process.env.GI_SCRET,
-    callbackURL: "http://localhost:4000/auth/github/callback"
-  })
+  new GithubStrategy(
+    {
+      clientID: process.env.GH_ID,
+      clientSecret: process.env.GH_SCRET,
+      callbackURL: `http://localhost:4000${routes.githubCallback}`
+    },
+    githubLoginCallback
+  )
 );
 
 passport.serializeUser(User.serializeUser());
