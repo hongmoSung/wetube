efed8b7a69b0b36110b0d5bcfbfa9a22081062d7
hongmo <tesung30@naver.com>
2019-03-24 18:44:22 +0900

    Recap and User Profile

--- controller/userController.js
+++ controller/userController.js
@@ -40,8 +40,7 @@ export const githubLogin = passport.authenticate("github");
 
 export const githubLoginCallback = async (_, __, profile, cb) => {
   const {
-    // eslint-disable-next-line camelcase
-    _json: { id, avatar_url, name, email }
+    _json: { id, avatar_url: avatarUrl, name, email }
   } = profile;
   try {
     const user = await User.findOne({ email });
@@ -54,7 +53,7 @@ export const githubLoginCallback = async (_, __, profile, cb) => {
       email,
       name,
       githubId: id,
-      avatarUrl: avatar_url
+      avatarUrl
     });
     return cb(null, newUser);
   } catch (error) {
@@ -69,6 +68,10 @@ export const postGithubLogIn = (req, res) => {
 export const logout = (req, res) => {
   req.logout();
   res.redirect(routes.home);
+};
+
+export const getMe = (req, res) => {
+  res.render("userDetail", { pageTitle: "User Detailt", user: req.user });
 };
 
 export const userDetail = (req, res) =>
